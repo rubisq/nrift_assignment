7274c3c0d6b5aaa768e42afb4d30d10f28627209
Subir <subir@pitangent.com>
2023-12-13 10:14:44 +0530

    Initial Commit

---
+++ nomura_assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nomura_assignment
+//
+//  Created by OSLT-0076 on 13/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
